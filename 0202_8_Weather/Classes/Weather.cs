2d7d277b7a3d5a2d22dc84c6184d3af17ae1c0bf
colov <colov@ZHIDKY>
2024-12-26 08:39:29 +0500

    добавлен класс погоды

---
+++ 0202_8_Weather/Classes/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _0202_8_Weather.Classes
+{
+    public class Weather
+    {
+        [Key]
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string JsonData { get; set; }
+        public DateTime RequestDate { get; set; }
+    }
+}
