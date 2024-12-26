091a61d71790baf2024f9a4a28a2d1d4f857b9a6
colov <colov@ZHIDKY>
2024-12-26 08:47:50 +0500

    Создание класса подключения

---
+++ 0202_8_Weather/Classes/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace _0202_8_Weather.Classes
+{
+    internal class Connection : DbContext
+    {
+        public DbSet<Weather> DataWeather { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql("server=localhost;database=0202pr8;user=root;pwd=Asdfg123", new MySqlServerVersion(new System.Version(8, 0, 11)));
+    }
+}
